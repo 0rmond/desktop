e44348c827b5a487dde3061f74ed46c8edb5f915
0rmond <88534988+0rmond@users.noreply.github.com>
2021-08-31 17:18:05 +0100

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo pacman -S firefox qtile python-pip neovim rxvt-unicode
+sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip neovim rxvt-unicode networkmanager git picom nitrogen 
+systemctl enable NetworkManager
