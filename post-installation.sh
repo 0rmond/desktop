a45c7e4f35eb93ca5778de452fc74e3a6d6b8d51
0rmond <88534988+0rmond@users.noreply.github.com>
2021-09-12 14:28:56 +0100

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip neovim rxvt-unicode networkmanager picom nitrogen 
 systemctl enable NetworkManager
-clone "https://github.com/0rmond/desktop/"
+pip install pywal
