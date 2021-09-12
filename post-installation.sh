c975783f44c16ba671662605edd872d23850bf71
0rmond <88534988+0rmond@users.noreply.github.com>
2021-09-12 20:27:04 +0100

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip neovim rxvt-unicode networkmanager picom nitrogen perl imagemagick
+sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip python-pywal neovim rxvt-unicode networkmanager picom nitrogen perl imagemagick
 systemctl enable NetworkManager
-pip install pywal colorthief haishoku colorz
+pip install wheel colorthief haishoku colorz
