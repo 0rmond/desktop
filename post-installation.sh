9034c1d82f6799cdf959d74ebc7e4a29470d564c
0rmond <88534988+0rmond@users.noreply.github.com>
2021-09-12 17:21:05 +0100

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip neovim rxvt-unicode networkmanager picom nitrogen 
+sudo pacman -S xf86-video-fbdev xorg xorg-xinit firefox qtile python-pip neovim rxvt-unicode networkmanager picom nitrogen perl
 systemctl enable NetworkManager
-pip install pywal
+pip install pywal colorthief haishoku colorz schemer2
